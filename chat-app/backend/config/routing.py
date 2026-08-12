959dd183a52809d4599d35211b3b8f268829d98d
Niranjan Reddy <niranjanreddynakkala@gmail.com>
2026-08-12 23:22:36 +0530

    debugging

--- chat-app/backend/config/routing.py
+++ chat-app/backend/config/routing.py
@@ -2,14 +2,16 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from django.urls import path
-
 from apps.chats.consumers import ChatConsumer
-
+print("🔥 ASGI ROUTING LOADED")
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
-    'websocket': AuthMiddlewareStack(
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
         URLRouter([
-            path('ws/chat/<int:conversation_id>/', ChatConsumer.as_asgi()),
+            path(
+                "ws/chat/<int:conversation_id>/",
+                ChatConsumer.as_asgi()
+            ),
         ])
     ),
 })
