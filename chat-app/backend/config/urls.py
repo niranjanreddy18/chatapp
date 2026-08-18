57e1a9a5e2f5f6346f8518f954352ddb4c9d9b1d
Niranjan Reddy <niranjanreddynakkala@gmail.com>
2026-08-18 21:38:06 +0530

    media url issue fixed

--- chat-app/backend/config/urls.py
+++ chat-app/backend/config/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     path('api/', include('apps.messages.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
